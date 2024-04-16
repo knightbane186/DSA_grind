53022f032c6c83cb58c1977f9af94945bf9d1a8a
knightbane186 <denverlopes93@gmail.com>
2024-04-17 00:44:15 +1000

    update3

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -2,3 +2,21 @@ let js = 'amazing';
 if (js === 'amazing') alert ("Javascript is fun");
 console.log(40+8);
 
+
+let firstName = "Jonas";
+// if its a constant only then write in upper case. 
+let PI = 3.1415;
+
+let myFirstJob = 'programmer';
+let myCurrentJob = 'Teacher';
+
+//assignment solution
+
+let country = 'India';
+let continent = 'Asia';
+let population = 33;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
