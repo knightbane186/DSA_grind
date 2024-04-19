338122bc50834ffd17219bd432e26670764d260f
knightbane186 <denverlopes93@gmail.com>
2024-04-19 19:54:19 +1000

    Quick update

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -8,4 +8,22 @@ const passTest = true;
 if(passTest) hasDriverLicense = true;
 if(hasDriverLicense) console.log('I can drive :D');
 
-const interface = 'Audio' 
+const interface = 'Audio'  
+
+function fruitProcessor(apple, oranges ){
+    const juice = ` Juice with ${apple} and oranges ${oranges} are the bed rock of our society`;
+    return juice;
+}
+
+
+function calcuAge1(birthYear){
+    return 2037-birthYear;
+}
+// this is called a anonymous fucntion or function without a name
+  const calcuAge2 = function (birthYr){
+return 20686-birthYr;    
+}
+
+console.log(age1,age2);
+ 
+
