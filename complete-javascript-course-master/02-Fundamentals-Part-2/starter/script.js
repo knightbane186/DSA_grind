64b6292464f743adf09d35a976593c317f360b0a
knightbane186 <denverlopes93@gmail.com>
2024-04-23 19:19:34 +1000

    Quick update

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -191,3 +191,28 @@ Looping backwards and forward
   dice = Math.trunc(Math.random() * 6)+1;
   if(dice === 6) console.log(`Loop is about to end...`);
  }
+
+ const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(bills[i] + tip);
+};
+
+
+//for loop
+for(let i = 0; i >= 10; i++){
+
+}
+//while loop
+while(something === something){
+
+}
