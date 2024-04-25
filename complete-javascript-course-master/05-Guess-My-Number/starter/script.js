899e1c03d8505b4b13f1d225ea50750259a34b1a
knightbane186 <denverlopes93@gmail.com>
2024-04-26 01:25:50 +1000

    Quick update

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -14,6 +14,8 @@
 //game logic
 
 const secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
+
 document.querySelector('.number').textContent = secretNumber ;
 
 
@@ -21,14 +23,36 @@ document.querySelector('.check').addEventListener('click', function(){
 
     //almost always the input we get from the web is in string so to convert it using Numbers. 
     const guess = Number(document.querySelector('.guess').value);
+    //when there is no input
 if (!guess){
     document.querySelector('.message').textContent = 'No Number';
 }else if(guess === secretNumber){
     document.querySelector('.message').textContent = 'Correct Number💫';
+//turining the page green:
+
+
+
+    //when player wins
 }else if(guess > secretNumber){
-    document.querySelector('.message').textContent = 'Too high!';
+    if(score > 1) {
+        document.querySelector('.message').textContent = 'Too high!';
+        score--;
+        document.querySelector('.score').textContent = score;
+        //when player too high
+    }else{
+        document.querySelector('.message').textContent = 'You lost the game';
+        document.querySelector('.score').textContent = 0;
+    }
+    //when player is too low
 }else if(guess < secretNumber){
-    document.querySelector('.message').textContent = 'Too low!';
+    if(score > 1) {
+        document.querySelector('.message').textContent = 'Too low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }else{
+        document.querySelector('.message').textContent = 'You lost the game';
+        document.querySelector('.score').textContent = 0;
+    }
 }
 
 });
