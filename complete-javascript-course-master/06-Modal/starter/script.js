fbdf3cad45602c852e748928b3965e8436a53bb8
knightbane186 <denverlopes93@gmail.com>
2024-04-30 01:38:48 +1000

    Quick update

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -4,25 +4,28 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal'); // Fixed typo: 'close-model' to 'close-modal'
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) { // Removed the semicolon at the end of the for loop
 
-    //this is when to pop open the modal
-    btnsOpenModal[i].addEventListener('click', function () {
-        console.log('Button clicked');
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
-
+const openModal = function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
 
     const closeModal = function(){
         modal.classList.add('hidden');
         overlay.classList.add('hidden');
        }
+for (let i = 0; i < btnsOpenModal.length; i++) { // Removed the semicolon at the end of the for loop
+
+    //this is when to pop open the modal
+    btnsOpenModal[i].addEventListener('click', openModal);
+
 
 //this is when we have to close
-   btnCloseModal.addEventListener('click',  );
+   btnCloseModal.addEventListener('click', closeModal);
+   overlay.addEventListener('click',closeModal);
+
 
 
 
