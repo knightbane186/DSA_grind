8775150d179aa890dfbd8d20fa15cc3f3d5e8409
knightbane186 <denverlopes93@gmail.com>
2024-04-29 16:39:19 +1000

    Quick update

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -16,7 +16,7 @@
 const secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber ;
+
 
 
 document.querySelector('.check').addEventListener('click', function(){
@@ -28,6 +28,7 @@ if (!guess){
     document.querySelector('.message').textContent = 'No Number';
 }else if(guess === secretNumber){
     document.querySelector('.message').textContent = 'Correct Number💫';
+    document.querySelector('.number').textContent = secretNumber ;
 //turining the page green:
 
 document.querySelector('body').style.backgroundColor = '#60b347';
@@ -40,6 +41,7 @@ document.querySelector('.number').style.width = '30rem';
         document.querySelector('.message').textContent = 'Too high!';
         score--;
         document.querySelector('.score').textContent = score;
+        // document.querySelector('body').style.backgroundColor = '#eee';
         //when player too high
     }else{
         document.querySelector('.message').textContent = 'You lost the game';
@@ -49,6 +51,7 @@ document.querySelector('.number').style.width = '30rem';
 }else if(guess < secretNumber){
     if(score > 1) {
         document.querySelector('.message').textContent = 'Too low!';
+        // document.querySelector('body').style.backgroundColor = '#eee';
         score--;
         document.querySelector('.score').textContent = score;
     }else{
@@ -57,59 +60,3 @@ document.querySelector('.number').style.width = '30rem';
     }
 }
 });
-
-else if(guess > secretNumber){
-    if(score > 1) {
-        document.querySelector('.message').textContent = 'Too high!';
-        score--;
-        document.querySelector('.score').textContent = score;
-        //when player too high
-    }else{
-        document.querySelector('.message').textContent = 'You lost the game';
-        document.querySelector('.score').textContent = 0;
-    }
-    //when player is too low
-}else if(guess < secretNumber){
-    if(score > 1) {
-        document.querySelector('.message').textContent = 'Too low!';
-        score--;
-        document.querySelector('.score').textContent = score;
-    }else{
-        document.querySelector('.message').textContent = 'You lost the game';
-        document.querySelector('.score').textContent = 0;
-    }
-}
-});
-
-
-}else if(guess < secretNumber){
-    if(score > 1) {
-        document.querySelector('.message').textContent = 'Too low!';
-        score--;
-        document.querySelector('.score').textContent = score;
-    }else{
-        document.querySelector('.message').textContent = 'You lost the game';
-        document.querySelector('.score').textContent = 0;
-    }
-}
-});
-
-
-
-document.querySelector('.message').textContent;
-
-document.querySelector('.message').textContent = '🦁Correct Number!';
-
-document.querySelector('.score').textContent = 10;
-document.querySelector('.number').textContent = 13;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value = 23);
-
-game logic
-
-
-
-
-
-
