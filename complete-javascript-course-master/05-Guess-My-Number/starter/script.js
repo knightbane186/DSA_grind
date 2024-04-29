844aa903059db2fb795722999e6cc1513f855c3c
knightbane186 <denverlopes93@gmail.com>
2024-04-29 17:27:33 +1000

    Quick update

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -15,7 +15,7 @@
 
 let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
-
+let highscore = 20;
 
 
 
@@ -34,6 +34,10 @@ if (!guess){
 document.querySelector('body').style.backgroundColor = '#60b347';
 
 document.querySelector('.number').style.width = '30rem';
+if(score > highscore){
+    highscore = score;
+    document.querySelector('.highscore').textContent = highscore;
+}
 
     //when player wins
 }else if(guess > secretNumber){
