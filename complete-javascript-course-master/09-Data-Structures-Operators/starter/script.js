34cbcfe6f5f7fa7d6736e42ecbb628c067cdf58d
knightbane186 <denverlopes93@gmail.com>
2024-05-02 14:04:25 +1000

    Quick update

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -4,7 +4,7 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
+// Data needed for the first part of the section
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -12,6 +12,11 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  // Fixed the function to avoid duplicate and correct 'startMenu' to 'starterMenu'
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +32,36 @@ const restaurant = {
     },
   },
 };
+
+
+//so this is creating a object 
+// const restaurant = {name,openingHours, categories};
+
+// and this is destructuring an already exisiting object nad pullin goput varriables from it
+const {name,openingHours, categories} = restaurant;
+
+//destructuring arrays
+
+// const arr = [2,3,4];
+
+// const a  = arr[0];
+// const b  = arr[0];
+// const c  = arr[0];
+
+
+// const[x,y,z] = arr;
+// console.log(x ,y, z);
+// console.log(arr);
+
+// const[first,second] = restaurant.categories;
+// console.log(first, second);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// const [starter, mainCourse] = restaurant.order(2,0);
+// console.log(starter,mainCourse);
+
+
+
+
