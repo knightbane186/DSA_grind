cb78dc9384d1b96c8adb4a4d3a5cf945ab58290a
knightbane186 <denverlopes93@gmail.com>
2024-05-03 01:18:55 +1000

    Quick update

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,21 @@
 'use strict';
 
 // // Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+
+  for(const flight of flights.split('+')){
+ const [type, from, to, time] = flight.split(';');
+ const output = `${type.startsWith('_Delayed') ? '🧀' : ''}${type.replaceAll('_',' ')} ${from.slice(0,3).toUpperCase()} ${to} (${time.replace(':','h')})`;
+ console.log(output);
+
+  }
+
+
+
+
+
 
 // // Data needed for the first part of the section
 // const restaurant = {
@@ -167,16 +180,18 @@
 
 // const question = new Map([
 //   []
-// ])
+// // ])
 
 
-const airline = 'Tap Air Portugal';
-const plane = 'A320';
+// const airline = 'Tap Air Portugal';
+// const plane = 'A320';
 
-console.log(plane[
+// console.log(plane[
 
 
-  document.querySelector('button')
+//   document.querySelector('button')  
+
+
 
 
 
