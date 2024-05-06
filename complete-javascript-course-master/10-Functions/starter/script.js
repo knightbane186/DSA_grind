ae03c406a69f27b159e304c5e559522ea9adda35
knightbane186 <denverlopes93@gmail.com>
2024-05-07 01:51:07 +1000

    Quick update

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,18 +1,44 @@
 'use strict';
 // We are learnig about default parameters 
 
-const bookings = [];
+// const bookings = [];
 
-const  createBooking = function(flightNUm,
-numPassenger = 1,price = 199){
+// const  createBooking = function(flightNUm,
+// numPassenger = 1,price = 199){
 
  
-    const booking = {
-        flightNUm,
-        numPassenger,
-        price
-    }
-    console.log(booking);
-    booking.push(booking);  
+//     const booking = {
+//         flightNUm,
+//         numPassenger,
+//         price
+//     }
+//     console.log(booking);
+//     booking.push(booking);  
+// }
+
+
+// createBooking('LH123');
+// createBooking('LH123',2,800);
+
+//
+
+const flight = 'LH234';
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 24739479284
 }
 
+const checkIn = function(flightNum, passenger){
+    flightNum = 'Lh999';
+    passenger.name = 'Mr.' + passenger.name;
+    if(passenger.passport ===24739479284 ){
+        alert('Check in')
+    }else{
+        alert('Wrong passport!')
+    }
+}
+
+const newPassport = function(person){
+    person.passport =Math.trunc(Math.random() * 10000);
+    
+}
