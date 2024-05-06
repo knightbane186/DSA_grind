4a25988c6596a3f2ef74ea363b582bdaeaa67457
knightbane186 <denverlopes93@gmail.com>
2024-05-06 23:11:25 +1000

    Quick update

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+// We are learnig about default parameters 
+
+const bookings = [];
+
+const  createBooking = function(flightNUm,
+numPassenger = 1,price = 199){
+
+ 
+    const booking = {
+        flightNUm,
+        numPassenger,
+        price
+    }
+    console.log(booking);
+    booking.push(booking);  
+}
+
