c21bc70b94481c3675777efa294121d393f22ab9
knightbane186 <denverlopes93@gmail.com>
2024-05-09 18:17:32 +1000

    Quick update

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -74,12 +74,63 @@ const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
 greetArr('Hi')('Jonas');
 
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+
+    book(flightNum,name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+        this.bookings.push({flight:`${this.iataCode}${flightNum}`,name});
+    },
+};
+
+const euroWings = {
+    name: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+
+};
+
+const book = lufthansa.book;
+
+book.call(euroWings,23,'Sarah wills');
+console.log(euroWings);
 
 
+const swiss = {
+    name: 
+}
+
+book.call(swiss,...flightData);
+
+//
+
+lufthansa.planes = 300;
+l;ufthansa.buyPlane = function(){
+    console.log(this);
+
+    this.planes++;
+    console.log(this.planes);
+}
+
+document.querySelector('.buy')
+.addEventListener('click',lufthansa. )
+
+//partially binding 
+const addTax = (rate,value) => value + value * rate;
+console.log(addTax(0.1,200));
+
+const addVAT = addTax.bind(null,0.23);
+
+console.log(addVAT(100));
+console.log(addVAT(23));
 
 
-
-
-
+const addTaxRate = function(rate){
+    return function(value) {
+        return value + value * rate;
+    }
+}
 
 
