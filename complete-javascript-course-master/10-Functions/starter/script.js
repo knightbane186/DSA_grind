587d1d8fde8ad745157f3f6b6c1ff5855beac0c1
knightbane186 <denverlopes93@gmail.com>
2024-05-09 12:08:52 +1000

    Quick update

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -22,48 +22,8 @@
 
 //
 
-const flight = 'LH234';
-const jonas = {
-    name: 'Jonas Schmedtmann',
-    passport: 24739479284
-}
-
-const checkIn = function(flightNum, passenger){
-    flightNum = 'Lh999';
-    passenger.name = 'Mr.' + passenger.name;
-    if(passenger.passport ===24739479284 ){
-        alert('Check in')
-    }else{
-        alert('Wrong passport!')
-    }
-}
-
-const newPassport = function(person){
-    person.passport =Math.trunc(Math.random() * 10000);
-    
-}
-
-
-// First class vs highher order function 
-// // Functions are just another type of objects in JS:
-// // Frist class citizens
-// //Functions are another "type" of object
-
-//  const add = (a,b) => a +b;
-  
-
 const oneWord = function(str){
-    return str.replace(/ /g, '').toLowerCase();
+    return str.replace(/ /g,'').toLowerCase();
 }
 
-const upperFirstWord = function(str){
-
-};
-
-const oneWord = function(str){
-    return str.replace(/ /g, '').toLowerCase();
-}
-
-const upperFirstWord = function(str){
-
-};
+const upperFirstWord = function
