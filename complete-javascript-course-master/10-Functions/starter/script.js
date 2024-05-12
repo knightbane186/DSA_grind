f5e7be963d22ef30cc732bdb27ba3813da81c2c9
knightbane186 <denverlopes93@gmail.com>
2024-05-12 17:58:34 +1000

    Quick update

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -167,18 +167,10 @@ const poll = {
     options: ['0: JavaScript', '1:Python','2:Rust','3: C++'],
 
     answers: new Array(4).fill(0),
+registeredNewAnswer(){
+    const answer = Number( prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
+},
 
-
-};
-
-
-const poll = {
-    question: 'What is your favourite progamming language?',
-    options: ['0: JavaScript', '1:Python','2:Rust','3: C++'],
-
-    answers: new Array(4).fill(0),
-
-    
 };
 
 
