32ea28c169b61b10003e82e1380736579ebe839c
knightbane186 <denverlopes93@gmail.com>
2024-05-13 15:23:38 +1000

    Quick update

--- complete-javascript-course-master/10-Functions/starter/closures.js
+++ complete-javascript-course-master/10-Functions/starter/closures.js
@@ -1,29 +1,32 @@
-'use strict';
+// 'use strict';
 
-//Closure
+// //Closure
 
-const secureBooking = function(){
-    let passengerCount = 0;
-     return function(){
-        passengerCount++;
-        console.log(`${passengerCount} passengers`);
-     };
-};
+// const secureBooking = function(){
+//     let passengerCount = 0;
+//      return function(){
+//         passengerCount++;
+//         console.log(`${passengerCount} passengers`);
+//      };
+// };
 
-const booker = secureBooking();
+// const booker = secureBooking();
 
-booker();
-booker();
-booker();
+// booker();
+// booker();
+// booker();
 
-// is closure is a closed-over variable environment of the execution context in which a function was created, even after that execution context is gone;
+// // is closure is a closed-over variable environment of the execution context in which a function was created, even after that execution context is gone;
 
-//A closure gives a function access to all variables of its parent function, even after that parent function has returned. The function keeps a reference to its outer scope, which preserves the scope chain throughout time. 
+// //A closure gives a function access to all variables of its parent function, even after that parent function has returned. The function keeps a reference to its outer scope, which preserves the scope chain throughout time. 
 
-//A closure makes sure that a function doesn't loose connection to variables that existed at the functions birth place;
+// //A closure makes sure that a function doesn't loose connection to variables that existed at the functions birth place;
 
-//To Dig into a closure;
+// //To Dig into a closure;
 
-console.dir(booker);
+// console.dir(booker);
+
+
+
 
 
