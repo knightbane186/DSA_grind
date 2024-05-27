559280cccb12cf6397e8858201d0361e7c15c72c
knightbane186 <denverlopes93@gmail.com>
2024-05-27 17:15:59 +1000

    Quick update

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+//constructor functions;
+//contructor functions always start with capital letters
+const Person = function(firstname,birthYear){
+
+}
+
+const jonas = new Person('Jonas',1992);
+
+console.log(jonas);
+
+//1.New{} is created
+//2.the new {} is called this, therefore, this = {}
+//3.now this is linked to prototype
+//4.Once the link is there, it will always return the {}
