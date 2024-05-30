de37de364ed725c936e949c9958e5d39f1d48109
knightbane186 <denverlopes93@gmail.com>
2024-05-30 17:13:41 +1000

    Quick update

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -2,8 +2,11 @@
 //constructor functions;
 //contructor functions always start with capital letters
 const Person = function(firstname,birthYear){
-
-}
+    this.firstname = firstname;
+    this.birthYear = birthYear;
+// this.calcAge = function(){
+//     console.log(2037- this.birthYear)
+};
 
 const jonas = new Person('Jonas',1992);
 
@@ -13,3 +16,20 @@ console.log(jonas);
 //2.the new {} is called this, therefore, this = {}
 //3.now this is linked to prototype
 //4.Once the link is there, it will always return the {}
+
+
+//so top keep it in a nutshell, what we created ont he top is a blueprint for the actual house and now we can just replicate. 
+const matilda = new Person('Matilda',2017);
+console.log(matilda);
+
+//prototype
+Person.prototype.calcAge = function(){
+    console.log(2037- this.birthYear);
+};
+
+console.log(jonas.__proto__);
+
+//prototype:
+Person.prototype.calcAge = function(){
+    console.log(2037 = this.birthYear)
+}
