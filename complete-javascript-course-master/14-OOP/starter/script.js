2d296c899bec1150b2663c521e39324ef1651275
knightbane186 <denverlopes93@gmail.com>
2024-06-14 17:58:55 +1000

    Quick update

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -194,3 +194,50 @@ class Person {
 const person = new Person('Alice', 30);
 console.log(person.getBirthYear()); // Accessing the result of the private method via a public method
 // console.log(person.#calculateBirthYear()); // SyntaxError: Private field '#calculateBirthYear' must be declared in an enclosing class
+
+class Person {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+    // Assuming some methods and properties of Person class
+}
+
+class Student extends Person {
+    university = 'University of Lisbon';
+    #studyHours = 0;
+    #course;
+    static numSubjects = 10;
+
+    constructor(fullName, birthYear, startYear, course) {
+        super(fullName, birthYear);
+        this.startYear = startYear;
+        this.#course = course;
+    }
+
+    introduce() {
+        console.log(`I study ${this.#course} at ${this.university}`);
+    }
+
+    study(h) {
+        this.#makeCoffee();
+        this.#studyHours += h;
+    }
+
+    #makeCoffee() {
+        return 'Here is a coffee for you';
+    }
+
+    get testScore() {
+        return this._testScore;
+    }
+
+    set testScore(score) {
+        this._testScore = score < 20 ? score : 0;
+    }
+
+    static printCurriculum() {
+        console.log(`There are ${this.numSubjects} subjects`);
+    }
+}
